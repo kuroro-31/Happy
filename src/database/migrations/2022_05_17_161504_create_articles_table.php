55cf8c924d3192593db14bcb760b184b929f0195
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-18 01:25:28 +0900

    migration追加

--- src/database/migrations/2022_05_17_161504_create_articles_table.php
+++ src/database/migrations/2022_05_17_161504_create_articles_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('body');
+            $table->unsignedbigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 };
