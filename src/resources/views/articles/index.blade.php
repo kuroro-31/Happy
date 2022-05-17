f39bb6159aafa6686ee7ef8386d1b938a8f8876a
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-18 01:02:23 +0900

    tailwindCSSの導入

--- src/resources/views/articles/index.blade.php
+++ src/resources/views/articles/index.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <body class="antialiased">
-        トップページ
+    <body>
+        <h1 class="5xl">トップページ</h1>
     </body>
 </html>
