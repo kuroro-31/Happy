b379a5eb093f6816b44a24ec14222e4b0ccea438
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-18 10:03:55 +0900

    認証後のリダイレクト修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Auth;
 */
 Auth::routes();
 Route::get('/', 'App\Http\Controllers\Articles\IndexController');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
