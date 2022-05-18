859dff08eda4386156413d11a5f80a3b07834396
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-18 12:26:15 +0900

    投稿ルーティングとコントローラーの作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,5 +14,15 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 Auth::routes();
+
 Route::get('/', 'App\Http\Controllers\Articles\IndexController');
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('articles')->name('articles.')->group(function () {
+    Route::post('/', 'App\Http\Controllers\Articles\StoreController')->name('store');
+    Route::get('/create', 'App\Http\Controllers\Articles\CreateController')->name('create');
+    Route::delete('/{article}', 'App\Http\Controllers\Articles\DestroyController')->name('destroy');
+    Route::patch('/{article}', 'App\Http\Controllers\Articles\UpdateController')->name('update');
+    Route::get('/{article}', 'App\Http\Controllers\Articles\ShowController')->name('show');
+    Route::get('/{article}/edit', 'App\Http\Controllers\Articles\EditController')->name('edit');
+    Route::put('/{article}/like', 'App\Http\Controllers\Articles\LikeController')->name('like')->middleware('auth');
+    Route::delete('/{article}/like', 'App\Http\Controllers\Articles\UnlikeController')->name('unlike')->middleware('auth');
+});
