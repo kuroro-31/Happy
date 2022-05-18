35339c81cc874a92bf6bd9755f2b091c4595a0fd
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-18 10:28:22 +0900

    bootstrap削除

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -10,7 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/js/app.js', 'public/js')
+    .postCss("resources/css/app.css", "public/css", [
+        require("tailwindcss"),
+    ])
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css',[])
