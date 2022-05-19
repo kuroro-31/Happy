cb077913e2639f48d0b1d2cd7190a87deaa448f9
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-19 23:20:21 +0900

    モデル修正

--- src/database/migrations/2022_05_17_161504_create_articles_table.php
+++ src/database/migrations/2022_05_17_161504_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('articles', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('title');
             $table->text('body');
             $table->unsignedbigInteger('user_id');
