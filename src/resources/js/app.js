cc2433a46589fdce0e4135f9fee6c5d253f1ffb0
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-22 21:47:11 +0900

    vue3に変更

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import './bootstrap'
 import ArticleLike from './components/ArticleLike'
 import ArticleTagsInput from './components/ArticleTagsInput'
 import FollowButton from './components/FollowButton'
 
-const app = new Vue({
-  el: '#app',
-  components: {
-    ArticleLike,
-    ArticleTagsInput,
-    FollowButton,
-  }
-})
+createApp({
+    components: {
+        'example-component': ExampleComponent,
+        'ArticleLike': ArticleLike,
+        'ArticleTagsInput': ArticleTagsInput,
+        'FollowButton': FollowButton,
+    },
+}).mount('#app')
