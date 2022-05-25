e21b035892446677e5d9618d05ae4f5f91b1a303
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-25 23:20:44 +0900

    レイアウト修正

--- src/resources/views/articles/index.blade.php
+++ src/resources/views/articles/index.blade.php
@@ -5,9 +5,28 @@
 @section('content')
   @include('atoms.nav')
   <div>
-    @foreach ($articles as $article)
-      @include('articles.card')
-    @endforeach
+
+    <div class="flex max-w-6xl w-full mx-auto px-8 justify-center">
+      <div class="md:w-1/5 relative h-screen py-8 pr-4">
+        <div class="fixed">
+          <a href="">aajjrgiaj
+
+          </a>
+        </div>
+      </div>
+      <div class="md:w-3/5 p-8">
+        @foreach ($articles as $article)
+          @include('articles.card')
+        @endforeach
+      </div>
+      <div class="md:w-1/5 relative h-screen py-8 pl-4">
+        <div class="fixed">
+          <a href="">aajjrgiaj
+
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
   {{-- {{ $articles->links() }} --}}
   {{-- {{ $articles->links('vendor/pagination/tailwind') }} --}}
