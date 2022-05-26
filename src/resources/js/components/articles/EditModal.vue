a6c2692f97c2fe9ee98ab1153e6503839264b442
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-26 23:11:29 +0900

    修正

--- src/resources/js/components/articles/EditModal.vue
+++ src/resources/js/components/articles/EditModal.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="relative">
+  <div class="relative" @mouseleave="open = false">
     <svg
-      data-modal-toggle="defaultModal"
       xmlns="http://www.w3.org/2000/svg"
       class="h-6 w-6 cursor-pointer"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
       stroke-width="2"
-      @click="open = !open"
+      @mouseover="open = true"
     >
       <path
         stroke-linecap="round"
@@ -16,23 +15,24 @@
         d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
       />
     </svg>
-    <div
-      v-show="open"
-      class="absolute top-0 right-0 w-48"
-      @click.self="open = false"
-    >
-      <div class="">
-        <span class="cursor-pointer" @click="edit">
-          <i class="fas fa-pen mr-1"></i>
-          記事を更新する
-        </span>
-        <div class="dropdown-divider"></div>
-        <a class="cursor-pointer" @click="destroy">
-          <i class="fas fa-trash-alt mr-1"></i>
-          記事を削除する
-        </a>
+    <transition>
+      <div
+        v-show="open"
+        class="absolute right-0 -top-2 w-48 z-50 rounded shadow-lg p-4 bg-white"
+      >
+        <div>
+          <span class="cursor-pointer" @click="edit">
+            <i class="fas fa-pen mr-1 hover:text-primary"></i>
+            記事を更新する
+          </span>
+          <div class="dropdown-divider"></div>
+          <span @click="destroy" class="cursor-pointer">
+            <i class="fas fa-trash-alt mr-1 hover:text-primary"></i>
+            記事を削除する
+          </span>
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 <script>
@@ -40,6 +40,9 @@ export default {
   data() {
     return {
       open: false,
+      errors: {},
+      success: false,
+      loaded: true,
     }
   },
   props: {
@@ -53,7 +56,11 @@ export default {
       window.location = `/articles/${this.articleId}/edit`
     },
     destroy() {
-      window.location = `/articles/${this.articleId}`
+      if (this.loaded) {
+        axios.delete(`/articles/${this.articleId}`).then(() => {
+          alert('成功！')
+        })
+      }
     },
   },
 }
