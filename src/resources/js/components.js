a6bb01915737ae55c2d890d785c8420a89b4f8ba
TARO <shun.ichiro.tanaka16@gmail.com>
2022-06-09 22:17:07 +0900

    js読み込み方の修正

---
+++ src/resources/js/components.js
@@ -0,0 +1,38 @@
+import { createApp } from 'vue'
+import ArticleLike from './components/ArticleLike'
+import CreateModal from './components/articles/CreateModal'
+import DeleteModal from './components/articles/DeleteModal'
+import EditModal from './components/articles/EditModal'
+import ArticleTagsInput from './components/ArticleTagsInput'
+import AuthModal from './components/auth/AuthModal'
+import CountAnimation from './components/CountAnimation'
+import FollowButton from './components/FollowButton'
+import HeaderNav from './components/HeaderNav'
+import HeaderUserModal from './components/HeaderUserModal'
+import Modal from './components/Modal'
+import ThemeToggle from './components/ThemeToggle'
+import AvatarZoom from './components/users/AvatarZoom'
+import EditUserModal from './components/users/EditUserModal'
+import FollowModal from './components/users/FollowModal'
+import ThumbnailZoom from './components/users/ThumbnailZoom'
+
+createApp({
+    components: {
+        ThumbnailZoom,
+        FollowModal,
+        AvatarZoom,
+        AuthModal,
+        DeleteModal,
+        CreateModal,
+        CountAnimation,
+        HeaderUserModal,
+        ThemeToggle,
+        EditModal,
+        EditUserModal,
+        ArticleLike,
+        ArticleTagsInput,
+        FollowButton,
+        HeaderNav,
+        Modal
+    },
+}).mount('#app')
