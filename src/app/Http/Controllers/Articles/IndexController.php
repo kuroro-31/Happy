2a9f5c43a4c48670968a4e09f47f0522bf316475
TARO <shun.ichiro.tanaka16@gmail.com>
2022-06-09 12:24:29 +0900

    フォロー中ユーザーの投稿を返す

--- src/app/Http/Controllers/Articles/IndexController.php
+++ src/app/Http/Controllers/Articles/IndexController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers\Articles;
 
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     /**
      * トップページ
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        $articles = Article::with('user')->latest()->paginate(20);
-        return view('articles.index', compact ('articles'));
+        // $articles = Article::with('user')->latest()->paginate(20);
+        
+        // フォロー中ユーザーの投稿を返す
+        $articles = Article::query()->whereIn('user_id', Auth::user()->followings()->pluck('followee_id'))->latest()->paginate(20);
+        return view(
+            'articles.index',
+            compact ('articles')
+        );
     }
 }
