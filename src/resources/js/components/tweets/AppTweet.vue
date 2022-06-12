6e613d8fdc148ddd2482d6b5a05a852a89bda9d6
TARO <shun.ichiro.tanaka16@gmail.com>
2022-06-13 00:38:07 +0900

    修正

--- src/resources/js/components/tweets/AppTweet.vue
+++ src/resources/js/components/tweets/AppTweet.vue
@@ -4,13 +4,16 @@
     >
         <!-- <component :is="`app-tweet-variant-${tweet.type}`" :tweet="tweet" /> -->
         <div class="flex w-full">
-            <div class="mr-3">
+            <a
+                :href="`/users/${tweet.user.username}`"
+                class="inline-block mr-3"
+            >
                 <img
                     :src="avatar"
                     alt="user avatar"
                     class="w-12 rounded-full"
                 />
-            </div>
+            </a>
             <div>
                 <app-tweet-username :user="tweet.user" />
                 <p class="text-gray-300 whitespace-pre-wrap">
--- src/resources/js/components/tweets/AppTweetUsername.vue
+++ src/resources/js/components/tweets/AppTweetUsername.vue
@@ -1,14 +1,19 @@
 <template>
-  <span class="text-gray-300 font-bold">{{ user.name }} <span class="text-gray-600 font-normal">@{{ user.username }}</span></span>
+    <a
+        :href="`/users/${user.username}`"
+        class="text-gray-300 font-bold cursor-pointer"
+        >{{ user.name }}
+        <span class="text-gray-600 font-normal">@{{ user.username }}</span></a
+    >
 </template>
 
 <script>
-  export default {
+export default {
     props: {
-      user: {
-        required: true,
-        type: Object
-      }
-    }
-  }
+        user: {
+            required: true,
+            type: Object,
+        },
+    },
+};
 </script>
