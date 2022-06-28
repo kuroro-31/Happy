cd979f30361a9011f9d2405314dbecb81b25f0fb
TARO <shun.ichiro.tanaka16@gmail.com>
2022-06-28 23:09:41 +0900

    修正

--- src/app/Http/Controllers/Books/Chapter/ShowController.php
+++ src/app/Http/Controllers/Books/Chapter/ShowController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Books\Chapter;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Chapter;
+use App\Models\Book;
 
 class ShowController extends Controller
 {
@@ -16,8 +17,10 @@ class ShowController extends Controller
      */
     public function __invoke($book, $chapter)
     {
-        $story = Chapter::where('id', $chapter)->first();
+        $story = Chapter::find($chapter);
+        $book = Book::find($book);
         return view('books.chapter.show', [
+            'book' => $book,
             'chapter' => $story,
         ]);
     }
