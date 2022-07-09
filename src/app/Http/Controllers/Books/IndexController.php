93d9b82eff8ae33c6083c026891474e27884e199
TARO <shun.ichiro.tanaka16@gmail.com>
2022-07-09 16:02:31 +0900

    一旦全ての本を返す

--- src/app/Http/Controllers/Books/IndexController.php
+++ src/app/Http/Controllers/Books/IndexController.php
@@ -16,38 +16,42 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $books = null;
-        $likeRankings = null;
+        // $books = null;
+        $books = Book::orderBy('created_at')->paginate(20);
+        // $likeRankings = null;
 
-        if (Auth::user()){
-            $ownPost = Book::where('user_id', Auth::user()->id)->first();
-            $followed = Auth::user()->followings()->pluck('followee_id')->first();
+        // if (Auth::user()){
+        //     $ownPost = Book::where('user_id', Auth::user()->id)->first();
+        //     $followed = Auth::user()->followings()->pluck('followee_id')->first();
 
-            if((empty($ownPost)) || (empty($followed)) || (empty($ownPost) && empty($followed))){
-                $likeRankings = Book::withCount('likes')
-                    ->orderBy('likes_count', 'desc')
-                    ->limit(20)
-                    ->get();
-            }
+        //     if((empty($ownPost)) || (empty($followed)) || (empty($ownPost) && empty($followed))){
+        //         $likeRankings = Book::withCount('likes')
+        //             ->orderBy('likes_count', 'desc')
+        //             ->limit(20)
+        //             ->get();
+        //     }
 
-            if ((empty($ownPost) && !empty($followed)) || (!empty($ownPost))){
-                $likeRankings = null;
-                    $books = Book::query()
-                    ->whereIn('user_id', Auth::user()->followings()->pluck('followee_id')) // フォロー中ユーザー
-                    ->orWhere('user_id', Auth::user()->id) // 自分
-                    ->latest() // 最新順
-                    ->paginate(20);
-            }
-        } else {
-            $likeRankings = Book::withCount('likes')
-                ->orderBy('likes_count', 'desc')
-                ->limit(20)
-                ->get();
-        }
+        //     if ((empty($ownPost) && !empty($followed)) || (!empty($ownPost))){
+        //         $likeRankings = null;
+        //             $books = Book::query()
+        //             ->whereIn('user_id', Auth::user()->followings()->pluck('followee_id')) // フォロー中ユーザー
+        //             ->orWhere('user_id', Auth::user()->id) // 自分
+        //             ->latest() // 最新順
+        //             ->paginate(20);
+        //     }
+        // } else {
+        //     $likeRankings = Book::withCount('likes')
+        //         ->orderBy('likes_count', 'desc')
+        //         ->limit(20)
+        //         ->get();
+        // }
 
         return view(
             'books.index',
-            compact ('books', 'likeRankings')
+            compact(
+                'books',
+                // 'likeRankings'
+            )
         );
     }
 }
