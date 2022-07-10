ea5f21b0d64b569395a7d21b7faa4b9972017d5a
TARO <shun.ichiro.tanaka16@gmail.com>
2022-07-10 16:42:25 +0900

    seeder修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -18,11 +18,19 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(100)->create();
 
         $faker = Faker::create('en_US');
-        for($i = 1; $i < 100; $i++){
+        for ($i = 1; $i < 100; $i++) {
             \App\Models\Book::create([
-                'title' => $faker->text(30),
+                'title' => $faker->text(15),
                 'body' => $faker->sentence,
                 'user_id' => random_int(1, 100)
+            ]);
+        }
+
+        for ($i = 1; $i < 1000; $i++) {
+            \App\Models\Chapter::create([
+                'name' => $faker->text(15),
+                'body' => $faker->text(1000),
+                'book_id' => random_int(1, 100)
             ]);
         }
     }
