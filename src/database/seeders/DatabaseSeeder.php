ec46b40bcf3f22c7603016d2b488b1c2f230db92
TARO <shun.ichiro.tanaka16@gmail.com>
2022-07-10 17:02:57 +0900

    seeder update

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(100)->create();
+        // \App\Models\User::factory(100)->create();
 
         $faker = Faker::create('en_US');
-        for ($i = 1; $i < 100; $i++) {
+        for ($a = 1; $a < 100; $a++) {
             \App\Models\Book::create([
                 'title' => $faker->text(15),
                 'body' => $faker->sentence,
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-        for ($i = 1; $i < 1000; $i++) {
+        for ($b = 1; $b < 1000; $b++) {
             \App\Models\Chapter::create([
                 'name' => $faker->text(15),
                 'body' => $faker->text(1000),
