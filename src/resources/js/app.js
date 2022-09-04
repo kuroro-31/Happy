415e65601e4ac6f0fe9cc757acdaf07f3d085986
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-09-04 19:26:46 +0900

    カラーテーマの設定

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import VueObserveVisibility from "vue-observe-visibility";
 import "./common/bootstrap";
 import components from "./common/components";
+import "./common/theme";
 import store from "./store";
 
 const app = createApp({
--- src/resources/js/components/ThemeToggle.vue
+++ src/resources/js/components/ThemeToggle.vue
@@ -30,17 +30,6 @@
 </template>
 <script>
 export default {
-    mounted() {
-        if (
-            localStorage.theme === "dark" ||
-            (!("theme" in localStorage) &&
-                window.matchMedia("(prefers-color-scheme: dark)").matches)
-        ) {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-    },
     methods: {
         changeTheme() {
             if (
