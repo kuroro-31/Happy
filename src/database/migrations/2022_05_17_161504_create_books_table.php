0f6683d6169bf39e7dad8d402bbab3b79c4c18a8
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-09-04 16:43:16 +0900

    投稿のコード追加

--- src/database/migrations/2022_05_17_161504_create_books_table.php
+++ src/database/migrations/2022_05_17_161504_create_books_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('code')->unique();
             $table->string('title');
             $table->text('body', 400);
             $table->string('thumbnail')->nullable();
