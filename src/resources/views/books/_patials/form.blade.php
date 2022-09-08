6324bed8b06b96b81f5a60181d887f09a71bf002
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-09-08 10:32:56 +0000

    作品モーダル修正

--- src/resources/views/books/_patials/form.blade.php
+++ src/resources/views/books/_patials/form.blade.php
@@ -5,6 +5,21 @@
         placeholder="30字以内で入力してください" required value="{{ $book->title ?? old('title') }}" maxlength="30">
 </div>
 <div class="mb-4">
+    <label for="author" class="text-xs text-666">原作</label>
+    <input type="text" name="author" class="w-full p-3 border border-ccc rounded-[3px] dark:bg-dark-2"
+        placeholder="" required value="{{ $book->author ?? old('author') }}" maxlength="30">
+</div>
+<div class="mb-4">
+    <label for="manga_artist" class="text-xs text-666">漫画家</label>
+    <input type="text" name="manga_artist" class="w-full p-3 border border-ccc rounded-[3px] dark:bg-dark-2"
+        placeholder="" required value="{{ $book->manga_artist ?? old('manga_artist') }}" maxlength="30">
+</div>
+<div class="mb-4">
+    <label for="assistant" class="text-xs text-666">アシスタント</label>
+    <input type="text" name="assistant" class="w-full p-3 border border-ccc rounded-[3px] dark:bg-dark-2"
+        placeholder="" required value="{{ $book->assistant ?? old('assistant') }}" maxlength="30">
+</div>
+<div class="mb-4">
     <label for="tag" class="text-xs text-666">タグ</label>
     <book-tags-input :initial-tags='@json($tagNames ?? [])' :autocomplete-items='@json($allTagNames ?? [])'>
     </book-tags-input>
