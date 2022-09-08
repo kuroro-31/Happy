5cb55461d4bce87d0f58c5c973c5e37fdad0bd3c
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-09-08 15:40:41 +0000

    タグ検索結果

--- src/resources/views/tags/show.blade.php
+++ src/resources/views/tags/show.blade.php
@@ -3,32 +3,29 @@
 @section('title', $tag->hashtag)
 
 @section('content')
-  @include('_patials._nav')
-  <div class="tag-hero">
-    <div class="z-30 absolute text-white font-semibold text-2xl flex justify-center">
-      <h2>{{ $tag->hashtag }}</h2>
-      <span class="inline-block ml-6">
-        {{ $tag->books->count() }}件
-      </span>
-    </div>
-    <div class="tag-hero-img">
-      <img class="" src="/img/balloon.jpeg" alt="">
-      <div class="tag-hero-img-bg"></div>
-    </div>
-  </div>
-  <div class="flex max-w-4xl w-full mx-auto px-8 justify-center">
-    <div class="md:w-2/5 relative h-screen py-8 pr-8 pr-4">
-      <div class="fixed">
-        <a href="">
-
-        </a>
-      </div>
-    </div>
-    <div class="md:w-3/5 py-8">
-      @foreach ($tag->books as $book)
-        @include('books._patials.card')
-      @endforeach
+    @include('_patials._transparent_nav')
+    <div class="tag-hero">
+        <div class="z-30 absolute text-white font-semibold flex flex-col items-center justify-center">
+            <h2 class="text-5xl my-4">{{ $tag->hashtag }}</h2>
+            <span class="inline-block text-2xl">
+                {{ $tag->books->count() }}件
+            </span>
+        </div>
+        <div class="tag-hero-img">
+            <div class="tag-hero-img-bg">
+                <img class="" src="/img/bg-2.svg" alt="">
+            </div>
+        </div>
     </div>
 
-  </div>
+    <div class="flex max-w-6xl w-full mx-auto mt-8 pt-8 px-12 md:px-0 justify-center">
+        <div class="w-full mx-12">
+            <div class="w-full flex flex-wrap justify-start">
+                @foreach ($tag->books as $book)
+                    @include('users._patials.card')
+                @endforeach
+            </div>
+        </div>
+    </div>
+
 @endsection
