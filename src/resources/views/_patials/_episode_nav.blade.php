0bcd354035798f6bbcf8844b0ccc4f7e23eb7004
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2022-09-11 02:27:27 +0000

    エピソードナブ修正

--- src/resources/views/_patials/_episode_nav.blade.php
+++ src/resources/views/_patials/_episode_nav.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed top-0 bg-dark w-full flex-none border-b border-ddd dark:border-dark">
+<div class="absolute top-0 bg-dark w-full">
     <div class="max-w-8xl mx-auto">
         <div class="py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
             <div class="relative flex items-center">
